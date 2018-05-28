c144db1e6cbf95307d76a1f516d684513c874f2f
Oliver Lee <oliverzlee@gmail.com>
2018-05-28 14:37:45 +0200

    Move Record information into SellRecord

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,11 @@ struct Record {
 
 #[derive(Clone, Debug)]
 struct SellRecord {
+    date_purchased: chrono::NaiveDate,
+    fund: String,
+    num_shares: f64,
+    share_price_purchased: f64,
+    share_price: f64,
     amount: f64,
     cap_gains: f64,
     cap_gains_ratio: f64,
@@ -90,12 +95,24 @@ impl Account {
 
         let mut vec = Vec::new();
         for record in &self.records {
-            let price = fund_prices.get(&record.fund).unwrap();
-            let amount = price * record.num_shares;
-            let cap_gains = (price - record.share_price) * record.num_shares;
+            let date_purchased = match chrono::NaiveDate::parse_from_str(&record.date, "%m/%d/%Y") {
+                Ok(date) => date,
+                Err(e) => return Err(AccountError(error::Error::description(&e).to_string())),
+            };
+            let fund = record.fund.clone(); // TODO use reference, lifetimes
+            let num_shares = record.num_shares;
+            let share_price_purchased = record.share_price;
+            let share_price = *fund_prices.get(&record.fund).unwrap();
+            let amount = share_price * num_shares;
+            let cap_gains = (share_price - share_price_purchased) * num_shares;
             let cap_gains_ratio = cap_gains / amount;
 
             vec.push(SellRecord {
+                date_purchased,
+                fund,
+                num_shares,
+                share_price_purchased,
+                share_price,
                 amount,
                 cap_gains,
                 cap_gains_ratio,
@@ -110,8 +127,8 @@ impl Account {
         fund_prices: &HashMap<String, f64>,
         sell_target: f64,
         tax_rate: f64,
-    ) -> Result<Vec<(Record, SellRecord)>, AccountError> {
-        let sell_records = self.make_sell_records(fund_prices)?;
+    ) -> Result<Vec<SellRecord>, AccountError> {
+        let mut sell_records = self.make_sell_records(fund_prices)?;
 
         let mut indices = Vec::new();
         for (i, item) in sell_records.iter().enumerate() {
@@ -119,18 +136,25 @@ impl Account {
         }
         indices.sort_unstable_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
+        let mut sell_records: Vec<Option<SellRecord>> =
+            sell_records.drain(..)
+                        .map(|s| Some(s))
+                        .collect();
+
         let mut amount = 0.0;
         let mut cap_gains = 0.0;
         let mut result = Vec::new();
         for (_, i) in indices {
-            let srec = &sell_records[i];
+            sell_records.push(None);
+            let srec = sell_records.swap_remove(i).unwrap();
             amount += srec.amount;
             cap_gains += srec.cap_gains;
 
+            // TODO: handle negative cap gains
             if (amount - cap_gains * tax_rate) > sell_target {
                 // see if we can sell some (not all) of the shares of this record
                 let mut x = srec.amount - srec.cap_gains * tax_rate;
-                x /= self.records[i].num_shares;
+                x /= srec.num_shares;
 
                 // get pre-record values for amount and cap gains
                 let a = amount - srec.amount;
@@ -140,29 +164,26 @@ impl Account {
                 // shares can only be sold as integer amounts
                 let n = ((sell_target - (a - c * tax_rate)) / x).trunc() + 1.0;
 
-                if n < self.records[i].num_shares.trunc() {
-                    result.push((
-                        Record {
-                            num_shares: n as f64,
-                            ..self.records[i].clone()
-                        },
+                if n < srec.num_shares.trunc() {
+                    result.push(
                         SellRecord {
-                            amount: srec.amount * n/self.records[i].num_shares,
-                            cap_gains: srec.cap_gains * n/self.records[i].num_shares,
-                            cap_gains_ratio: srec.cap_gains_ratio
+                            num_shares: n,
+                            amount: srec.share_price*n,
+                            cap_gains: (srec.share_price - srec.share_price_purchased)*n,
+                            ..srec
                         }
-                    ));
+                    );
                 } else {
-                    result.push((self.records[i].clone(), srec.clone()));
+                    result.push(srec);
                 }
                 break;
             } else {
-                result.push((self.records[i].clone(), srec.clone()));
+                result.push(srec);
             }
         }
 
         if amount < sell_target {
-            return Err(AccountError(format!("Insufficient funds.")));
+            return Err(AccountError("Insufficient funds.".to_string()));
         }
 
         Ok(result)
@@ -189,24 +210,19 @@ fn load_account(filename: &String) -> Result<Account, Box<error::Error>> {
     Ok(Account::new(vec))
 }
 
-fn print_sell_summary(mut summary: Vec<(Record, SellRecord)>, tax_rate: f64) {
-    summary.sort_unstable_by(|a, b| {
-        let date_a = chrono::NaiveDate::parse_from_str(&a.0.date, "%m/%d/%Y").unwrap();
-        let date_b = chrono::NaiveDate::parse_from_str(&b.0.date, "%m/%d/%Y").unwrap();
-        date_b.cmp(&date_a)
-    });
-
+fn print_sell_summary(mut summary: Vec<SellRecord>, tax_rate: f64) {
+    summary.sort_unstable_by(|a, b| b.date_purchased.cmp(&a.date_purchased));
     println!("Selling the following records:");
 
     let mut amount = 0.0;
     let mut cap_gains = 0.0;
-    for (record, sell_record) in summary {
+    for srec in summary {
         println!(
             "  {},\t{},\t{} shares",
-            record.date, record.fund, record.num_shares
+            srec.date_purchased, srec.fund, srec.num_shares
         );
-        amount += sell_record.amount;
-        cap_gains += sell_record.cap_gains;
+        amount += srec.amount;
+        cap_gains += srec.cap_gains;
     }
 
     println!("will result in");
